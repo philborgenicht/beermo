f94a581d1c4178363e6ec5537296bd4d168795d0
Phillip Borgenicht <philborgenicht@gmail.com>
2018-12-13 14:27:16 -0800

    route files

--- routes/beers.js
+++ routes/beers.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex')
+var knex = require('../knex')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  knex('beers')
+  .then(data=>{
+    res.send(data)
+  })
+})
+
+router.get('/:id', function(req, res, next){
+  knex('beers')
+  .where('id', req.params.id)
+  .then(data=>{
+    res.send(data[0])
+  })
+})
+
+router.post('/', function(req, res, next){
+  knex('beers')
+  .insert(req.body)
+  .returning('*')
+  .then(data=>{
+    res.send(data)
+  })
+})
+
+
+
+
+
 
 module.exports = router;
