3fa05d8d5a477d1a11d633c71feaa4d4c195246a
William Brown <wrbrown3530@gmail.com>
2018-12-13 13:11:22 -0700

    testing

--- routes/beers.js
+++ routes/beers.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex')
+var knex = require('../knex')
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  return knex('beers')
-.select('beer_name', 'favor_value')
+  knex('beers')
+//.select('beer_name', 'favor_value')
 .then((beers) => {
-  res.status(200).send(beers)
+  res.send(beers)
 })
 })
 
