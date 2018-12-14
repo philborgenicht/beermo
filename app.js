0dc56435d403b06582fbc4573b108edb6e06c2aa
Mark Fantini <strictlybusinessemail242@gmail.com>
2018-12-14 14:26:12 -0700

    Added separate signup route with bcrpyt.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var usersRouter = require('./routes/users');
-var transactionsRouter = require('./routes/transactions');
-var beersRouter= require('./routes/beers');
-var favorsRouter= require('./routes/favors');
-var favoritesRouter= require('./routes/favorites');
+let usersRouter = require('./routes/users');
+let transactionsRouter = require('./routes/transactions');
+let beersRouter= require('./routes/beers');
+let favorsRouter= require('./routes/favors');
+let favoritesRouter= require('./routes/favorites');
+let signupRouter = require('./routes/signup')
 
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/signup', signupRouter)
 app.use('/users', usersRouter);
 app.use('/beers', beersRouter);
 app.use('/transactions', transactionsRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let router = express.Router();
 let knex = require('../knex')
+const jwt = require('jsonwebtoken')
 let bcrypt = require('bcryptjs')
-let salt = bcrypt.genSaltSync(10)
 
 
 /* GET users listing. */
@@ -22,19 +22,56 @@ router.get('/:id', function(req,res,next){
   })
 })
 
-router.post('/', function(req,res,next){
-  let passWord = req.body.hashed_password
-  let hash = bcrypt.hashSync(passWord, 8);
-  let body = req.body
-  body[ 'hashed_password' ] = hash;
+router.post('/:id', function(req,res,next){
+  let id=req.params.id
+
+  const { email, hashed_password } = req.body
+  if (!email || !email.trim()) {
+    return next({ status: 400, message: 'Email must not be blank' })
+  }
+
+  if (!hashed_password || !hashed_password.trim()) {
+    return next({ status: 400, message: 'Password must not be blank' })
+  }
+
+  let user
 
   knex('users')
-  .insert(req.body)
-  .returning(['first_name','last_name','email'])
-  .then(data=>{
-    res.send(data)
-  })
+  .where('id',id)
+  .first()
+  .then(row=>{
+      if (!row) {
+        return next({ status: 400, message: 'Bad email or password' })
+      }
+
+      user = row
+
+      return bcrypt.compare(hashed_password, user.hashed_Password)
+    })
+    .then((data) => {
+      if (!data) {
+        return next({ status: 400, message: 'Bad email or password' })
+      }
+      const claim = { userId: user.id }
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '7 days'  // Adds an exp field to the payload
+      })
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),  // 7 days
+        secure: router.get('env') === 'production'  // Set from the NODE_ENV
+      })
+
+      delete user.hashedPassword
+      res.send(user)
+    })
+
+    .catch((err) => {
+      next(err)
+    })
 })
+
 
 router.patch('/:id', function(req, res, next){
   knex('users')
