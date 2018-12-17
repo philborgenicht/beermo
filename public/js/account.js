10a71c6cb184e8296e37fce8abd3b21fb2bfe5fa
Mark Fantini <strictlybusinessemail242@gmail.com>
2018-12-17 12:00:04 -0700

    Completed the account history for beer recipients.

--- public/js/account.js
+++ public/js/account.js
@@ -10,21 +10,33 @@ const appendage=document.getElementById('acctinfo')
     if(typeof verified.data !== 'number'){
       return window.open('./aboutus.html','_self')
     }
-
+    let id = verified.data
     getMyAccount(id)
     .then( results => {
-      console.log(results.data)
       for (var i = 0; i < results.data.length; i++) {
         let li = document.createElement('li')
 
+        let brian
+
+        if (results.data[i].sender===false) {
+          brian = `You Favored Them`
+        }else{
+          brian = `They Favored You`
+        }
+
         li.innerText =
-        `Transaction Id: ${results.data[i].id},
+        `Them: ${results.data[i].sender_first_name} ${results.data[i].sender_last_name},
 
         Beer Earned: ${results.data[i].beer_id},
 
-        Favor Performed: ${results.data[i].favor}
+        Favor Performed: ${results.data[i].favor},
+
+        ${brian},
+
+        Transaction Date: ${results.data[i].created_at.substring(0,10)}
 
         `
+
 
         appendage.appendChild(li)
       }
