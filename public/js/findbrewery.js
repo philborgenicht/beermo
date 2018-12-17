6bae1524834257873b590d507261b995ccb7fb32
William Brown <wrbrown3530@gmail.com>
2018-12-17 09:52:14 -0700

    changes to findbrewery.js

--- public/js/findbrewery.js
+++ public/js/findbrewery.js
@@ -5,10 +5,9 @@ let searchButton = document.getElementById("search")
 searchButton.addEventListener('click', function(event){
   let userText = document.getElementById("textarea1")
   axios.get(`https://api.openbrewerydb.org/breweries?by_state=${userText.value}`)
- console.log(userText.value)
    .then(function(response) {
-     axios.get(response.data.results[0].name)
-     console.log(response.data.results[0].name)
+     axios.get(response.data)
+     console.log(response.data)
    })
    .catch(function(error) {
       console.log('error')
