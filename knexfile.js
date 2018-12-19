77b6bb2c8af04b41fc3461d42219032c5bcb56f0
Phillip Borgenicht <philborgenicht@gmail.com>
2018-12-19 11:04:42 -0800

    brian

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: "postgres://wcueuscvkkrotd:4fff2468d15911ee8f090291b967b0240b052bb162e5c5dd42ff8569dc20bb1d@ec2-107-20-237-78.compute-1.amazonaws.com:5432/d6vra9hmilc44o"
+
   },
 }
